2b68d337344904347e63faa799b4af1b5e36374b
Wirakarn <wirakarn.ang@gmail.com>
2026-07-17 23:18:53 +0700

    second postgresql submission

--- 02_querying_assessment/postgresql/bonus/query_task1_bonus.mongodb.js
+++ 02_querying_assessment/postgresql/bonus/query_task1_bonus.mongodb.js
@@ -17,4 +17,19 @@
 // Write in English or Thai. Do not skip this step.
 //
 // Your thinking:
-//
+// The owner needs a list of every menu item in the 'Side' category along with its price, 
+// so I'll use the menu_items collection, filtering for category: "Side" and 
+// projecting only the name and price fields since the designer needs.
+// - collection used: menu_items (name, price, category)
+// - MongoDB used: 
+//      - find () to filter the document where 'category' = 'side'
+//      - Projection return only name and price, setting _id: 0 to exclude
+
+// Write a query on the menu_items collection to find the name and price
+// of all documents where the category is 'Side'.
+use("chrome-burger-db");
+
+db.menu_items.find(
+    { category: "Side" },
+    { name: 1, price: 1, _id: 0 }
+)
--- 02_querying_assessment/postgresql/bonus/query_task2_bonus.mongodb.js
+++ 02_querying_assessment/postgresql/bonus/query_task2_bonus.mongodb.js
@@ -17,4 +17,18 @@
 // Write in English or Thai. Do not skip this step.
 //
 // Your thinking:
-//
+// The manager needs to contact every staff member whose role is 'Cook'. 
+// I'll use the staff collection, filtering for role: "Cook" since the manager needs to identify and contact them.
+// - collection used: staff (first_name, last_name, role)
+// - MongoDB used: 
+//      - find () to match document where role = "Cook"
+//      - Projection return only first_name and last_name, setting _id: 0 to exclude
+
+// Write a query on the staff collection to find the first_name and last_name
+// of all documents where the role is 'Cook'.
+use("chrome-burger-db");
+
+db.staff.find(
+    { role: "Cook" },
+    { first_name: 1, last_name: 1, _id: 0 }
+)
